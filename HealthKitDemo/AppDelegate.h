d0ece1203796fa8a36d8d0df8ef9b8057096334b
李晓璐 <vast0608@163.com>
2018-03-01 09:13:50 +0800

    Initial Commit

---
+++ HealthKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthKitDemo
+//
+//  Created by 李晓璐 on 2018/3/1.
+//  Copyright © 2018年 onmmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthKitDemo
+//
+//  Created by 李晓璐 on 2018/3/1.
+//  Copyright © 2018年 onmmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
